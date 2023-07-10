2c45ccd65f3812a6f0f4ba571b4f6a3847dd3dbe
judsykid <139158761+judsykid@users.noreply.github.com>
2023-07-10 14:26:09 -0300

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -73,8 +73,8 @@ export const managers = [
     },
     {
       // "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID": "Dillon Wight",
-      "name": "Your Name",
+      "managerID": "461576869548912640",
+      "name": "Dillon Wight",
       // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn", // (optional)
       "bio": "Lorem ipsum...",
